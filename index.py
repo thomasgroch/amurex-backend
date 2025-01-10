f69caee6bb01175a48ff74c376c0fc4fff135671
Sanskar Jethi <sansyrox@gmail.com>
2025-01-10 17:20:48 +0000

    fix: another decode issue

--- index.py
+++ index.py
@@ -1020,7 +1020,7 @@ async def track(request: Request, body: TrackingRequest):
 async def close(ws, msg):
     meeting_id = ws.query_params.get("meeting_id")
     primary_user_key = f"primary_user:{meeting_id}"
-    if redis_client.get(primary_user_key).decode() == ws.id:
+    if redis_client.get(primary_user_key) == ws.id:
         redis_client.delete(primary_user_key)
 
     return ""
